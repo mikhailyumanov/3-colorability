38aa668ba6ad83bfd344f51687468ca489db13f3
Mikhail Yumanov <yumanov_m@mail.ru>
2021-01-04 22:27:39 +0300

    Simplify simple algorithm

--- src/main/java/com/mikhailyumanov/three_colorability/algorithm/solvers/SimpleRandomizedSolver.java
+++ src/main/java/com/mikhailyumanov/three_colorability/algorithm/solvers/SimpleRandomizedSolver.java
@@ -4,55 +4,95 @@ import com.mikhailyumanov.three_colorability.csp_instance.CSPInstance;
 import com.mikhailyumanov.three_colorability.csp_instance.Constraint;
 import com.mikhailyumanov.three_colorability.csp_instance.VarColor;
 import com.mikhailyumanov.three_colorability.csp_instance.Variable;
+import com.mikhailyumanov.three_colorability.modifier.instructions.Lemma2RemoveVariable;
 import com.mikhailyumanov.three_colorability.modifier.instructions.RemoveColor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SimpleRandomizedSolver extends AbstractSolver {
   public boolean hasColoring(CSPInstance instance) {
-    return loop(instance);
-  }
-
-  private boolean loop(CSPInstance instance) {
     do {
       modifier.flushAppliedChanges();
       applyLemmas2to6(instance);
     } while (modifier.hasRecentlyChanged());
 
+    return loop(instance);
+  }
+
+  private boolean loop(CSPInstance instance) {
+    if (instance.getConstraints().isEmpty() || instance.getVariable_ids().size() <= 3) {
+      return hasColoringBruteForce(instance);
+    }
+
+    for (int i = 0; i < instance.getVariable_ids().size(); i++) {
+      for (int j = i + 1; j < instance.getVariable_ids().size(); j++) {
+        int varId1 = instance.getVariable_ids().get(i);
+        int varId2 = instance.getVariable_ids().get(j);
+        List<Constraint> constraints = instance.getConstraints().stream()
+            .filter(constraint ->
+                constraint.getFirst().getVariableId() == varId1 &&
+                constraint.getSecond().getVariableId() == varId2 ||
+                constraint.getFirst().getVariableId() == varId2 &&
+                constraint.getSecond().getVariableId() == varId1).collect(Collectors.toList());
+        if (constraints.isEmpty()) {
+          continue;
+        }
+
+        Constraint constraint = constraints.get(0);
+        VarColor varColor1 = constraint.getFirst();
+        VarColor varColor2 = getPossibleColors(instance, constraint.getSecond()).get(0);
+
+        CSPInstance copyInstance = CSPInstance.copyFrom(instance);
+        modifier.stepBy(copyInstance, new RemoveColor(copyInstance, varColor1));
+        modifier.stepBy(copyInstance, new RemoveColor(copyInstance, varColor2));
+        modifier.stepBy(copyInstance, new Lemma2RemoveVariable(copyInstance,
+            copyInstance.getVariable(varColor1.getVariableId())));
+        modifier.stepBy(copyInstance, new Lemma2RemoveVariable(copyInstance,
+            copyInstance.getVariable(varColor2.getVariableId())));
+
+        if (loop(copyInstance)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private boolean hasColoringBruteForce(CSPInstance instance) {
+    List<Variable> variables = instance.getVariables();
+    if (variables.size() != instance.getVariable_ids().size()) {
+      return false;
+    }
+
     if (instance.getConstraints().isEmpty()) {
-      return instance.getVariables().size() == instance.getVariable_ids().size();
+      // cases variables.size() == 0 and variables.size() == 1 are included here
+      return true;
     }
 
-    Constraint constraint = instance.getConstraints().get(instance.getConstraints().size() - 1);
-
-    VarColor varColor1 = constraint.getFirst();
-    VarColor varColor2 = constraint.getSecond();
-    List<VarColor> possibleColors1 = getPossibleColors(instance, varColor1);
-    List<VarColor> possibleColors2 = getPossibleColors(instance, varColor2);
-
-    if (possibleColors1.size() != 2 || possibleColors2.size() != 2) {
-      System.err.println(instance);
-      assert false;
+    if (variables.size() == 2) {
+      for (VarColor varColor1 : variables.get(0).getVarColors()) {
+        for (VarColor varColor2 : variables.get(1).getVarColors()) {
+          if (!instance.getConstraints().contains(new Constraint(varColor1, varColor2))) {
+            return true;
+          }
+        }
+      }
     }
 
-    List<List<VarColor>> removedPairs = List.of(
-        List.of(varColor1, possibleColors2.get(0)),
-        List.of(varColor1, possibleColors2.get(1)),
-        List.of(varColor2, possibleColors1.get(0)),
-        List.of(varColor2, possibleColors1.get(1))
-    );
-
-    for (List<VarColor> pair : removedPairs) {
-      VarColor fromConstraint = pair.get(0);
-      VarColor other = pair.get(1);
-
-      CSPInstance copyInstance = CSPInstance.copyFrom(instance);
-      modifier.stepBy(copyInstance, new RemoveColor(copyInstance, fromConstraint));
-      modifier.stepBy(copyInstance, new RemoveColor(copyInstance, other));
-
-      if (loop(copyInstance)) {
-        return true;
+    if (variables.size() == 3) {
+      for (VarColor varColor1 : variables.get(0).getVarColors()) {
+        for (VarColor varColor2 : variables.get(1).getVarColors()) {
+          for (VarColor varColor3 : variables.get(2).getVarColors()) {
+            if (!instance.getConstraints().contains(new Constraint(varColor1, varColor2)) &&
+                !instance.getConstraints().contains(new Constraint(varColor1, varColor3)) &&
+                !instance.getConstraints().contains(new Constraint(varColor2, varColor3))) {
+              return true;
+            }
+          }
+        }
       }
     }
 
